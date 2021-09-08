4eab7c933315cd51291013225d31b581a936a571
Katiana Kontolati <kkontol1@jhu.edu>
2021-09-09 01:31:58 +0300

    Update bootstrap_PLS_example.py

--- bootstrap_PLS_example.py
+++ bootstrap_PLS_example.py
@@ -1,3 +1,8 @@
+# LICENSE
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC for the U.S. Department of Energy/National Nuclear Security Administration. All rights are reserved by Triad National Security, LLC, and the U.S. Department of Energy/National Nuclear Security Administration. The U.S. Government has granted for itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide license in this material to reproduce, prepare derivative works, distribute copies to the public, perform publicly, display publicly, and to permit others to do so.
+
+#Permission is granted to the public to copy, use, modify, and distribute this material without charge, provided that the Notice in its entirety, including without limitation the statement of reserved government rights, are reproduced on all copies.
+
 
 # %%
 from sklearn.cross_decomposition import PLSRegression
